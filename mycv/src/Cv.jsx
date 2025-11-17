3d2693cd7062181f33c3de0589e8a767bc5baecf
Mikko <yuxin.29.wu@gmail.com>
2025-11-17 23:01:20 +0200

    project order changed

--- mycv/src/Cv.jsx
+++ mycv/src/Cv.jsx
@@ -5,15 +5,17 @@ function Cv() {
     return (
         <div id="Cv">
             <h1 style={{ textAlign: "left" }}>Yuxin Wu</h1>
-            <p>
+            <h2>
                 Architect (5+ years) → Junior Software Developer
-            </p>
+            </h2>
             <p>
                 📧 yuxin.1.wu@gmail.com | 📞 +358 466803054 | 📍 Helsinki, Finland
             </p>
             <p>
                 <a href="in/yuxin-w-ab719b198" target="_blank" rel="noreferrer">LinkedIn</a> |{" "}
-                <a href="https://github.com/Yuxin29" target="_blank" rel="noreferrer">GitHub</a>
+                <a href="https://github.com/Yuxin29" target="_blank" rel="noreferrer">GitHub</a> |{" "}
+                <a href="https://www.hive.fi/about-hive/" target="_blank" rel="noreferrer"> Trained at Hive</a>
+                
             </p>
 
             <h2>About</h2>
@@ -26,24 +28,29 @@ function Cv() {
             <p>-Collaborate across multidisciplinary teams</p>
             <p>-Deliver precise, reliable, and efficient technical solutions under tight deadlines</p>
             
+            <h2>About Hive</h2>
+            <p>
+                Hive is an immersive tech school where students develop problem-solving, collaboration, and self-learning skills through hands-on projects. Its rigorous, project-based curriculum equips students with strong programming fundamentals and real-world technical expertise. Students learn to explore, experiment, and take ownership of their work while tackling complex challenges.
+            </p>
+
             <h2>Tech Stacks</h2>
-            <h4>Programming Languages <span>C, C++, Python, Bash, Makefile</span> </h4>
-            <h4>Tools <span>Linux, Virtual Machines, Debian, Git</span> </h4>
-            <h4>Offices <span>Microsoft, Slack, Discord</span> </h4>
-            <h4>Design <span>AutoCAD, 3D modeling, rendering, visualization, Adobe Suite, Laser-cutting, 3D printing</span> </h4>
-            <h4>Communications <span>Chinese(native), English(Excellent), Finnish(conversational) </span> </h4>
+            <h4><b>Programming Languages</b> <span>C, C++, Python, Bash, Makefile</span> </h4>
+            <h4><b>Tools</b> <span>Linux, Virtual Machines, Debian, Git</span> </h4>
+            <h4><b>Offices</b> <span>Microsoft, Slack, Discord</span> </h4>
+            <h4><b>Design</b> <span>AutoCAD, 3D modeling, rendering, visualization, Adobe Suite, Laser-cutting, 3D printing</span> </h4>
+            <h4><b>Communications</b> <span>Chinese(native), English(Excellent), Finnish(conversational) </span> </h4>
             
+            <h2 style={{ textAlign: "left" }}>Education</h2>
+            <h4><b>Hive Helsinki, Finland</b> <span> Software Engineering 04.2024 – now</span></h4>
+            <h4><b>Aalto University, Finland</b> <span> Master of Science in Architecture  2019–2021</span></h4>
+            <h4><b>Southeast University, China</b> <span> Bachelor of Science in Architecture  2014–2019</span></h4>
+            <h4><b>Iowa State University, USA</b> <span> Exchange study 2018</span></h4>
+
             <h2>Work Experience</h2>
             <h4>Project Architect <span>– Architecture Office Eero Korhonen Oy, Helsinki (1.2023 – now)</span> </h4>
             <h4>Architect <span>– Architecture Office Pet Michael Oy, Helsinki (1.2022 – 12.2022)</span> </h4>
             <h4>Assistant Architect <span>– H-K Architects, Helsinki (1.2023 – now)</span> </h4>
             <h4>Assistant Architect <span>– Powerhouse Company, Helsinki (1.2023 – now)</span> </h4>
-            
-            <h2 style={{ textAlign: "left" }}>Education</h2>
-            <h4>Hive Helsinki, Finland <span> Software Engineering 04.2024 – now</span></h4>
-            <h4>Aalto University, Finland <span> Master of Science in Architecture  2019–2021</span></h4>
-            <h4>Southeast University, China <span> Bachelor of Science in Architecture  2014–2019</span></h4>
-            <h4>Iowa State University, USA <span> Exchange study 2018</span></h4>
 
         </div>
     );
--- mycv/src/Project.jsx
+++ mycv/src/Project.jsx
@@ -17,56 +17,48 @@ function Project() {
             <div className="project-card">
                 <img
                     className="project-media"
-                    src={libftImage}
+                    src={webserverImage}
                     alt="Libft project preview"
                 />
 
                 <div className="project-text">
                     <h4>
-                        <a href="https://github.com/Yuxin29/Mini_C_Library.git"
-                           target="_blank"
-                           rel="noreferrer"
-                           style={{ textDecoration: "none", color: "inherit" }}>
-                           <b>MiniLibft (C)</b>
-                        </a>
-                        <span> — self-implemented C standard library functions.</span>
+                        <b>Webserver (C++)</b>
+                        <span> Webserver built from scratch.</span>
                     </h4>
                     <p>
-                        MiniLibft is a project where I reimplemented several core functions from the C standard library. The goal was to understand the inner workings of common library functions such as malloc, free, strcpy, strlen, and more. This project provided me with a deep understanding of memory management and the importance of efficient, error-free coding when dealing with low-level operations.
+                        WebServer is a lightweight HTTP server implemented in C++ that mimics the core behavior of real-world web servers. In a team of three, forcucing individually on the server infrastructure, configuration parsing and full request–response cycle(me), including HTTP request parsing, routing logic, and response generation.                      
+                        Throughout this project, Ilearned a great deal about teamwork, Git branch management, and coordinating development workflows in a multi-developer environment.
                     </p>
                 </div>
             </div>
 
-
             <div className="project-card">
                 <video
                     className="project-video"
-                    src={pushswapVideo}
+                    src={miniRTVideo}
                     controls
                     loop
                     muted
                     playsInline
                     preload="metadata"
                 >
-                    Your browser does not support the video tag.
                 </video>
-
                 <div className="project-text">
                     <h4>
                         <a href="https://github.com/Yuxin29/my_miniRT.git"
                            target="_blank"
                            rel="noreferrer"
                            style={{ textDecoration: "none", color: "inherit" }}>
-                           <b>Radix Sort (C)</b>
+                           <b>MiniRayTracer (C)</b>
                         </a>
-                        <span> — sorting challenge using two stacks and minimal operations.</span>
+                        <span> Created a ray tracer to explore 3D rendering algorithms.</span>
                     </h4>
                     <p>
-                        Push_Swap is an algorithmic C project where I wrote a solver that sorts a sequence of integers using only two stacks and the allowed operations (sa, sb, ss, pa, pb, ra, rb, rr, rra, rrb, rrr). I implemented robust input parsing and error handling, small-input optimized routines, and a scalable strategy (radix) for larger inputs. The project strengthened my skills in algorithm design, complexity analysis, low-level C programming, and writing tests that verify correctness and measure move-efficiency.
+                        MMiniRayTracer is a lightweight ray tracer built using C and the MLX42 graphics library. The project involves rendering simple 3D scenes by simulating the behavior of light rays as they interact with objects. I implemented basic features such as ray-object intersection, reflection, and simple lighting effects. This project helped me improve my problem-solving skills in computer graphics and algorithms for 3D rendering.
                     </p>
                 </div>
             </div>
-
 
             <div className="project-card">
                 <img
@@ -83,10 +75,10 @@ function Project() {
                            style={{ textDecoration: "none", color: "inherit" }}>
                            <b>MiniShell (C)</b>
                         </a>
-                        <span> — Self-implemented Bash shell.</span>
+                        <span> Self-implemented Bash shell.</span>
                     </h4>
                     <p>
-                        MiniShell is a simplified version of the Bash shell built in C. It includes essential features like executing commands, handling pipes, and performing input/output redirection. The project focuses on building a functional shell with command parsing, handling of special symbols, and user input management. It allowed me to improve my understanding of system calls, process management, and file descriptors in a Unix-based environment.
+                        MiniShell is a simplified version of the Bash shell. It includes essential features like executing commands, handling pipes, and performing input/output redirection. The project focuses on building a functional shell with command parsing, handling of special symbols, and user input management. It allowed me to improve my understanding of system calls, process management, and file descriptors in a Unix-based environment.
                     </p>
                 </div>
             </div>
@@ -94,28 +86,26 @@ function Project() {
             <div className="project-card">
                 <video
                     className="project-video"
-                    src={miniRTVideo}
+                    src={pushswapVideo}
                     controls
                     loop
                     muted
                     playsInline
                     preload="metadata"
                 >
-                    Your browser does not support the video tag.
                 </video>
-
                 <div className="project-text">
                     <h4>
                         <a href="https://github.com/Yuxin29/my_miniRT.git"
                            target="_blank"
                            rel="noreferrer"
                            style={{ textDecoration: "none", color: "inherit" }}>
-                           <b>MiniRayTracer (C)</b>
+                           <b>Radix Sort (C)</b>
                         </a>
-                        <span> — Created a ray tracer to explore 3D rendering algorithms.</span>
+                        <span> Sorting challenge using two stacks and minimal operations.</span>
                     </h4>
                     <p>
-                        MMiniRayTracer is a lightweight ray tracer built using C and the MLX42 graphics library. The project involves rendering simple 3D scenes by simulating the behavior of light rays as they interact with objects. I implemented basic features such as ray-object intersection, reflection, and simple lighting effects. This project helped me improve my problem-solving skills in computer graphics and algorithms for 3D rendering.
+                        Push_Swap is an algorithmic C project where I wrote a solver that sorts a sequence of integers using only two stacks and the allowed operations (sa, sb, ss, pa, pb, ra, rb, rr, rra, rrb, rrr). I implemented robust input parsing and error handling, small-input optimized routines, and a scalable strategy (radix) for larger inputs. The project strengthened my skills in algorithm design, complexity analysis, low-level C programming, and writing tests that verify correctness and measure move-efficiency.
                     </p>
                 </div>
             </div>
@@ -123,18 +113,20 @@ function Project() {
             <div className="project-card">
                 <img
                     className="project-media"
-                    src={webserverImage}
+                    src={libftImage}
                     alt="Libft project preview"
                 />
-
                 <div className="project-text">
                     <h4>
-                        <b>Webserver (C++)</b>
-                        <span> — C++ Web Server built from scratch.</span>
+                        <a href="https://github.com/Yuxin29/Mini_C_Library.git"
+                           target="_blank"
+                           rel="noreferrer"
+                           style={{ textDecoration: "none", color: "inherit" }}>
+                           <b>MiniLibft (C)</b> </a>
+                        <span> self-implemented C standard library functions.</span>
                     </h4>
                     <p>
-                        WebServer is a lightweight HTTP server implemented in C++ that mimics the core behavior of real-world web servers. In a team of three, forcucing individually on the server infrastructure, configuration parsing and full request–response cycle(me), including HTTP request parsing, routing logic, and response generation.                      
-                        Throughout this project, Ilearned a great deal about teamwork, Git branch management, and coordinating development workflows in a multi-developer environment.
+                        MiniLibft is a project where I reimplemented several core functions from the C standard library. The goal was to understand the inner workings of common library functions such as malloc, free, strcpy, strlen, and more. This project provided me with a deep understanding of memory management and the importance of efficient, error-free coding when dealing with low-level operations.
                     </p>
                 </div>
             </div>
@@ -160,7 +152,7 @@ function Project() {
                            style={{ textDecoration: "none", color: "inherit" }}>
                            <b>CoinCollector(Python)</b>
                         </a>
-                        <span> — a game where a robot collects coins, escapes ghosts. </span>
+                        <span> a game where a robot collects coins, escapes ghosts. </span>
                     </h4>
                     <p> . </p>
                     <p>
